6fc708bad7dce34f16d6f83fa5d95c78a93f0566
Yugen1121 <yugenlimbu777@gmail.com>
2025-11-15 14:01:25 +0000

    Initialised the child class of node. Represents City.

---
+++ NES/src/util/nodes/City.java
@@ -0,0 +1,26 @@
+package util.nodes;
+
+import java.util.Map;
+
+import util.ServiceLinkedList;
+
+/*
+ * Represents a City object in the system 
+ * <p>
+ * This is a child class of Node. It defines the node Type "City" and uses inherited functions and variables 
+ * from the parent class. 
+ * </p> 
+ */
+
+public class City extends Node{
+
+	
+	@Override
+	public String getNodeType() {
+		return "City";
+		
+	}
+	
+	
+
+}
