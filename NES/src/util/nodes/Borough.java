0ef59af88be037550b81f883e2269ca5323cecf7
Yugen1121 <yugenlimbu777@gmail.com>
2025-11-15 14:01:49 +0000

    Initialised a child class of Node. Represents Borough.

---
+++ NES/src/util/nodes/Borough.java
@@ -0,0 +1,24 @@
+package util.nodes;
+
+import java.util.Map;
+
+import util.ServiceLinkedList;
+
+/*
+ * Represents a Borough object in the system 
+ * <p>
+ * This is a child class of Node. It defines the node Type "Borough" and uses inherited functions and variables 
+ * from the parent class. 
+ * </p> 
+ */
+
+public class Borough extends Node {
+
+
+	@Override
+	public String getNodeType() {
+		return "Borough";
+		
+	}
+
+}
