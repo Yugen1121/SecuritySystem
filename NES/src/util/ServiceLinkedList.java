76a214d7450e315d5968030aa4038dde5b65a3d9
Yugen1121 <yugenlimbu777@gmail.com>
2025-11-24 15:46:54 +0000

    added a aavailibility variable and its getter and setter

--- NES/src/util/ServiceLinkedList.java
+++ NES/src/util/ServiceLinkedList.java
@@ -2,6 +2,7 @@ package util;
 import util.nodes.*;
 public class ServiceLinkedList {
 	private Service value;
+	private boolean available = true; 
 	private ServiceLinkedList next = null;
 	
 	public ServiceLinkedList(Service x) {
@@ -18,5 +19,11 @@ public class ServiceLinkedList {
 	
 	public void removeNext() {
 		this.next = null;
+	}
+	public void switchAvailability() {
+		this.available = !this.available;
+	}
+	public Service getValue() {
+		return this.value;
 	}
 }
