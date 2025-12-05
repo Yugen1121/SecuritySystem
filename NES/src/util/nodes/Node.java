b600863e1e4292e0a3194457c6118bc112e0a90b
Yugen1121 <yugenlimbu777@gmail.com>
2025-12-05 09:55:42 +0000

    added a new function to return incident rate of each type of incident and made write functions synchronized.

--- NES/src/util/nodes/Node.java
+++ NES/src/util/nodes/Node.java
@@ -6,8 +6,11 @@ import java.util.Map;
 import util.ServiceLinkedList;
 import util.Service;
 import util.nodes.CustomDSA.NeighbourNode;
+import util.nodes.CustomDSA.TreeNode;
 import util.*;
 import java.util.HashSet;
+import java.util.ArrayList;
+
 /*
  * This is the base entity for anything related to Node/Location in the system.
  * 
@@ -32,13 +35,22 @@ import java.util.HashSet;
  * </ul>
  */
 
-public abstract class Node {
+public abstract class Node{
 	private int id;
 	protected String LocationName;
 	protected Map<String, ServiceLinkedList> availableService;
 	protected PriorityQueue<NeighbourNode> Neighbours = new PriorityQueue<>((a, b) -> Float.compare(a.getDist(), b.getDist()));
 	protected Boolean Incident = true;
+	protected Map<Integer, ArrayList<Incident>> incidents;
 	
+	public Node(int id, String LocationName) {
+		this.id = id;
+		this.LocationName = LocationName;
+		incidents.put(1, new ArrayList<Incident>());
+		incidents.put(2, new ArrayList<Incident>());
+		incidents.put(3, new ArrayList<Incident>());
+	}
+
 	abstract String getNodeType();
 	
 	public String getLocation() {
@@ -53,16 +65,26 @@ public abstract class Node {
 		return this.id;
 	}
 	
+	public PriorityQueue<NeighbourNode> getNeighbour(){
+		return this.Neighbours;
+	}
+	
+	public Map<String, ServiceLinkedList> getServices(){
+		return this.availableService;
+	}
+	
 	public void addService(Service x) {
-		if (this.availableService.containsKey(x.getServiceType())) {
-			String Type = x.getServiceType();
-			ServiceLinkedList list = this.availableService.get(Type);
-			ServiceLinkedList ToAdd = new ServiceLinkedList(x);
-			ToAdd.setNext(list);
-			this.availableService.put(Type, ToAdd);
-			
-		}else {
-			this.availableService.put(x.getServiceType(), new ServiceLinkedList(x));
+		synchronized(this){
+			if (this.availableService.containsKey(x.getServiceType())) {
+				String Type = x.getServiceType();
+				ServiceLinkedList list = this.availableService.get(Type);
+				ServiceLinkedList ToAdd = new ServiceLinkedList(x);
+				ToAdd.setNext(list);
+				this.availableService.put(Type, ToAdd);
+				
+			}else {
+				this.availableService.put(x.getServiceType(), new ServiceLinkedList(x));
+			}
 		}
 	}
 	
@@ -72,12 +94,14 @@ public abstract class Node {
 	}
 	
 	public Service popService(String Type) {
-		if (this.availableService.containsKey(Type)) {
-			ServiceLinkedList n = this.availableService.get(Type);
-			this.availableService.put(Type, n!=null? n.getNext(): null);
-			return n.getValue();
+		synchronized(this) {
+			if (this.availableService.containsKey(Type)) {
+				ServiceLinkedList n = this.availableService.get(Type);
+				this.availableService.put(Type, n!=null? n.getNext(): null);
+				return n.getValue();
+			}
+			return null;
 		}
-		return null;
 	}
 	
 	public Map<String, ServiceLinkedList> getAvailableServices(){
@@ -85,232 +109,33 @@ public abstract class Node {
 	}
 	
 	public void removeServiceById(Service node) {
-		ServiceLinkedList dummy = this.availableService.get(node.getServiceType());
-		if (dummy==null) return;
-		if (dummy.getValue() == node || node.getID() == dummy.getValue().getID()) {
-			this.availableService.put(dummy.getValue().getServiceType(),dummy.getNext());
-		}
-		else {
-			while (dummy.getNext() != null) {
-				Service n = dummy.getNext().getValue(); 
-				if ( n == node || node.getID() == n.getID()) {
-					dummy.setNext(dummy.getNext().getNext());
-					break;
-				}
-				dummy = dummy.getNext();
+		synchronized(this) {
+			ServiceLinkedList dummy = this.availableService.get(node.getServiceType());
+			if (dummy==null) return;
+			if (dummy.getValue() == node || node.getID() == dummy.getValue().getID()) {
+				this.availableService.put(dummy.getValue().getServiceType(),dummy.getNext());
 			}
-		}
-	}
-	
-	
-	private void MakeRequest(int incidentType, int EmergencyLevel) {
-		Map<String, Integer> requestedServices = new HashMap<>();
-		if (EmergencyLevel >= 9) {
-			// Police 5
-			requestedServices.put(Police.Type, 5);
-			// If fire
-			if (incidentType == 1) {
-				requestedServices.put(FireTruck.Type, 2);
-				requestedServices.put(Ambulance.Type, 3);
-			}
-			// if injury / attack
-			else if(incidentType == 2) {
-				requestedServices.put(Ambulance.Type, 3);
-			}
-		}
-		else if(EmergencyLevel >= 7) {
-			// Police 4
-			requestedServices.put(Police.Type, 4);
-			// If fire
-			if (incidentType == 1) {
-				requestedServices.put(FireTruck.Type, 2);
-				requestedServices.put(Ambulance.Type, 2);
-						}
-			// if injury / attack
-			else if(incidentType == 2) {
-				requestedServices.put(Ambulance.Type, 3);
-		}
-
-
-		}
-		else if(EmergencyLevel >= 5) {
-			// Police 3
-			requestedServices.put(Police.Type, 3);
-			// If fire
-			if (incidentType == 1) {
-				requestedServices.put(FireTruck.Type, 1);
-				requestedServices.put(Ambulance.Type, 2);
-			}
-			// if injury / attack
-			else if(incidentType == 2) {
-				requestedServices.put(Ambulance.Type, 2);
-			}
-
-		}
-		else if(EmergencyLevel >= 3) {
-			// police 2
-			requestedServices.put(Police.Type, 2);
-			// If fire
-			if (incidentType == 1) {
-				requestedServices.put(FireTruck.Type, 1);
-				requestedServices.put(Ambulance.Type, 1);
-			}
-			// if injury / attack
-			else if(incidentType == 2) {
-				requestedServices.put(Ambulance.Type, 1);
-			}
-
-		}
-		else {
-			// Police 1
-			requestedServices.put(Police.Type, 1);
-			// If fire
-			if (incidentType == 1) {
-				requestedServices.put(FireTruck.Type, 1);
-				requestedServices.put(Ambulance.Type, 1);
-			}
-			// if injury / attack
-			else if(incidentType == 2) {
-				requestedServices.put(Ambulance.Type, 1);
-			}
-
-		}
-		this.SearchForServices(requestedServices);
-		
-	}
-	
-	public Map<String, Integer> DispatchServices(Map<String, Integer> s, NeighbourNode Node) {
-		Node x = Node.getNode();
-		
-		for (String map : s.keySet()) {
-			// checks if the node has the services
-			if (x.getAvailableServices().containsKey(map)) {
-				
-				ServiceLinkedList head = x.availableService.get(map);
-				while (head!=null && s.get(map) > 0) {
-					if (head.getValue().getAvailability()) {
-						head = head.getNext();
-						s.put(map, s.get(map)-1);
-						//need to use run function here
-					}
-					else {
+			else {
+				while (dummy.getNext() != null) {
+					Service n = dummy.getNext().getValue(); 
+					if ( n == node || node.getID() == n.getID()) {
+						dummy.setNext(dummy.getNext().getNext());
 						break;
 					}
+					dummy = dummy.getNext();
 				}
-				x.availableService.put(map, head);
-				if (head==null || s.get(map) <= 0) continue;
-				ServiceLinkedList curr = head;
-				
-				while (curr.getNext() != null && s.get(map)>0) {
-					ServiceLinkedList n = curr.getNext();
-					if (n.getValue().getAvailability()) {
-						curr.setNext(n.getNext());
-						s.put(map, s.get(map)-1);
-					}
-					else {
-						curr = curr.getNext();
-					}
-				}
-				
-				
 			}
 		}
-		
-		return s;
+	}	
+	
+	// returns a the incident rate of each kind of incident.
+	public Map<Integer, Float> getIncidentRate() {
+		Map<Integer, Float> rate = new HashMap<>();
+		for (int x: this.incidents.keySet()) {
+			float number = this.incidents.get(x).size()/100; 
+			rate.put(x, number);
+		}
+		return rate;
 	}
-	
-	public Map<String, Integer> DispatchServices(Map<String, Integer> s) {
-		for (String map : s.keySet()) {
-			// checks if the node has the services
-			if (s.containsKey(map)) {
-				ServiceLinkedList ahead = this.availableService.get(map);
-				ServiceLinkedList behind = this.availableService.get(map);
-				ServiceLinkedList head = this.availableService.get(map);
-				// checking if the services that is in this  node are available
-				while (ahead != null && s.get(map) > 0) {
-					// head is available then we remove it from the list
-					if (ahead.getValue().getAvailability()) {
-						if (ahead == head) {
-							head = head.getNext();
-							behind = head;
-							ahead.removeNext();
-							ahead = head;
-						}
-						else {
-							behind.removeNext();
-							behind.setNext(ahead.getNext());
-							ahead.removeNext();
-							ahead = behind.getNext();
-						}
-						s.put(map, s.get(map)-1);
-					}
-					else {
-						if (behind == ahead) {
-							ahead = ahead.getNext();
-						}
-						else {
-							behind = ahead;
-							ahead = ahead.getNext();
-						}
-					}
-				}
-				if (s.get(map) <= 0) {
-					s.remove(map);
-				}
-				this.availableService.put(map, head);
-			
-			}
-		}
-
-		return s;
-	}
-	
-	public void SearchForServices(Map<String, Integer> s) {
-		// check if the node itself has the services needed
-		// iterating through the list of services that are needed
-		s = this.DispatchServices(s);	
-		if (!s.isEmpty()) {
-			PriorityQueue<NeighbourNode> heap = this.makeDuplicate(new NeighbourNode(this, 0), this.Neighbours);
-			NeighbourNode node = heap.poll();
-			HashSet<Integer> visited = new HashSet<>();
-			while (!s.isEmpty() && node != null) {
-				if (!visited.contains(node.getNode().getID())) {
-					visited.add(node.getNode().getID());
-					s = this.DispatchServices(s, node);
-					if(s.isEmpty()) {
-						break;
-					}
-					
-					this.makeDuplicate(node, heap);
-				}
-				node = heap.poll();
-			}
-			
-		}
-		
-		return;
-		
-	};
-	
-	private PriorityQueue<NeighbourNode> makeDuplicate(NeighbourNode parent, PriorityQueue<NeighbourNode> heap) {
-		// add the copy to the heap with increased distance 
-		for (NeighbourNode i:  new PriorityQueue<>(parent.getNode().Neighbours)) {
-			// makes sure the closed road is avoided
-			if (i.getOpen()) {
-				float dist = i.getDist()+parent.getDist();
-				Node n = i.getNode();
-				// if the road is congested then the distance is assumed to be 1.1 times the original
-				if (i.getCongested()) {
-					dist *= 1.1;
-				}
-				NeighbourNode y = new NeighbourNode(n, dist);
-				y.setParent(parent);
-				heap.add(y);
-			}
-		}
-		return heap;
-	}
-	
-	
 	
 }
