ebc9e8062a8f7abf12d2b3aab5a36430a49341e2
Yugen1121 <yugenlimbu777@gmail.com>
2025-12-19 10:31:52 +0000

    Bug fixes.

--- NES/src/util/Environment.java
+++ NES/src/util/Environment.java
@@ -6,17 +6,47 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.PriorityQueue;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableMap;
+
 import java.util.ArrayList;
 import util.Service;
 public class Environment {
-	private Map<Integer, Node> nodes = new HashMap<>();
-	private Map<Integer, Service> services = new HashMap<>();
-	private Map<Integer, Incident> incident = new HashMap<>();
+	private final ObservableMap<Integer, Node> nodes = FXCollections.observableHashMap();
+	private final ObservableMap<Integer, Service> services = FXCollections.observableHashMap();
+	private final ObservableMap<Integer, Incident> incident = FXCollections.observableHashMap();
 	private PriorityQueue<Incident> runningIncidents = new PriorityQueue<Incident>(
 			(a, b) -> Integer.compare(b.getIncidentLevel(), a.getIncidentLevel())
 			);
 	
+	public Environment() {
+		
+	}
 	
+	public void update(Boolean updateAll) {
+		if (updateAll) {
+			for (Node n: this.nodes.values()) {
+				n.recalculateServiceRequired();
+			}
+		}
+		for (Service n: this.services.values()) {
+			n.run();
+		}
+		return;
+	}
+	
+	public int getNewServiceId() {
+		return services.size() + 1;
+	}
+	
+	public void addService(Service s) {
+		services.put(s.getID(), s);
+	}
+	public Map<Integer, Node> getNodes(){
+		return this.nodes;
+	}
+
 	public void addNode(int id, Node node) {
 		this.nodes.put(id, node);
 	}
@@ -26,9 +56,11 @@ public class Environment {
 		Incident New = new Incident(incident.size()+1, node, EmergencyLevel, incidentType);
 		this.incident.put(New.getId(), New);
 		this.runningIncidents.add(New);
-		
+		node.addToIncidents(New);
+		node.addToRunningIncident(New);
 		Map<String, Integer> requestedServices = new HashMap<>();
 		if (EmergencyLevel >= 9) {
+			
 			// Police 5
 			requestedServices.put(Police.Type, 5);
 			// If fire
@@ -40,8 +72,10 @@ public class Environment {
 			else if(incidentType == 2) {
 				requestedServices.put(Ambulance.Type, 3);
 			}
+			
 		}
 		else if(EmergencyLevel >= 7) {
+			
 			// Police 4
 			requestedServices.put(Police.Type, 4);
 			// If fire
@@ -52,10 +86,12 @@ public class Environment {
 			// if injury / attack
 			else if(incidentType == 2) {
 				requestedServices.put(Ambulance.Type, 3);				
-			} 	
+			}
+			
 		}
 		else if(EmergencyLevel >= 5) {
 			// Police 3
+			
 			requestedServices.put(Police.Type, 3);
 			// If fire
 			if (incidentType == 1) {
@@ -66,9 +102,10 @@ public class Environment {
 			else if(incidentType == 2) {
 				requestedServices.put(Ambulance.Type, 2);
 			}
-
+			
 		}
 		else if(EmergencyLevel >= 3) {
+			
 			// police 2
 			requestedServices.put(Police.Type, 2);
 			// If fire
@@ -80,9 +117,10 @@ public class Environment {
 			else if(incidentType == 2) {
 				requestedServices.put(Ambulance.Type, 1);
 			}
-
+			
 		}
 		else {
+			
 			// Police 1
 			requestedServices.put(Police.Type, 1);
 			// If fire
@@ -94,7 +132,7 @@ public class Environment {
 			else if(incidentType == 2) {
 				requestedServices.put(Ambulance.Type, 1);
 			}
-
+			
 		}
 		this.SearchForServices(requestedServices, New);
 		
@@ -104,27 +142,32 @@ public class Environment {
 	public NeighbourNode flipStartPos(NeighbourNode node) {
 		ArrayList<NeighbourNode> ls = new ArrayList<>();
 		ArrayList<Float> ds = new ArrayList<>();
+		
 		while (node != null) {
+			//[n1,n2,n2,n3,dest
 			ls.add(new NeighbourNode(node));
+			//[ 43,33,22,11,0]
 			ds.add(node.getDist());
 			node = node.getParent();
 		}
 		
-		// linkes new nodes
-		for (int i = 0; i < ls.size()-1; i++) {
-			ls.get(i).setParent(ls.get(i+1));
-		}
-	
+		for (int i = 0; i < ls.size() - 1; i++) {
+	        ls.get(i).setParent(ls.get(i + 1));
+	    }
+	    ls.get(ls.size() - 1).setParent(null);
+		
+		// start from the end 
 		float prev = 0;
-		for (int i = ds.size()-2; i > -1; i--) {
+		for (int i = 1; i < (ls.size()); i++) {
 			NeighbourNode n = ls.get(i);
 			float x = ds.get(i);
-			float y = ds.get(i+1);
-			float newDist = y-x + prev;
+			float y = ds.get(i-1);
+			float newDist = y - x + prev;
 			prev = newDist;
 			n.setDist(newDist);
 		}
-		NeighbourNode head = ls.get(ls.size()-1); 
+		
+		NeighbourNode head = ls.get(0); 
 		head.setDist(0);
 		return head;
 	}
@@ -143,13 +186,19 @@ public class Environment {
 		for (int i = ls.size()-1; i > 0; i--) {
 			// point the node at index i to node at index i-1
 			ls.get(i).setParent(ls.get(i-1));
+			
 		}
 		ls.get(0).setParent(null);
 		return ls.get(ls.size()-1);
 	}
 	
 	public Map<String, Integer> DispatchServices(Map<String, Integer> s, NeighbourNode node, Incident incident) {
-		Map<String, Map<Integer, Service>> mainMap = node.getNode().getServices();
+		
+		Map<String, Map<Integer, Service>> mainMap = new HashMap<>(node.getNode().getServices());
+		for (String st: mainMap.keySet()) {
+			mainMap.put(st, new HashMap<>(mainMap.get(st)));
+		}
+		
 		for (String key : s.keySet()) {
 			// checks if the node has the services
 			
@@ -161,8 +210,9 @@ public class Environment {
 					while (it.hasNext() && s.get(key) > 0){
 						Service curr = services.get(it.next()); 
 						if (curr.available) {
-							curr.setPath(flipStartPos(node));
-							curr.setUnAvailable();
+							
+							curr.allocate(flipStartPos(node), incident.getIncidentLevel());
+							
 							incident.addToDispatchList(key, curr.getID());
 							it.remove();
 							s.put(key, s.get(key)-1);
@@ -182,6 +232,8 @@ public class Environment {
 		// check if the node itself has the services needed
 		// iterating through the list of services that are needed
 		Node n = incident.getNode();
+		
+
 		PriorityQueue<NeighbourNode> hp = new PriorityQueue<>(
 				(a, b) -> Float.compare(a.getDist(), b.getDist())
 				);
@@ -210,11 +262,13 @@ public class Environment {
 			
 		}
 		
+
 		return;
 		
 	};
 	
-	private PriorityQueue<NeighbourNode> makeDuplicate(NeighbourNode parent, PriorityQueue<NeighbourNode> heap) {
+	private PriorityQueue<NeighbourNode> makeDuplicate(NeighbourNode start, PriorityQueue<NeighbourNode> heap) {
+		NeighbourNode parent = new NeighbourNode(start);
 		// add the copy to the heap with increased distance 
 		for (NeighbourNode i:  new PriorityQueue<>(parent.getNode().getNeighbour())) {
 			// makes sure the closed road is avoided
@@ -233,15 +287,27 @@ public class Environment {
 		return heap;
 	}
 	
+	public void incidentDelt(Incident inc) {
+		inc.getNode().removeRunningIncident(inc);
+		Map<String, ArrayList<Integer>> dsList = inc.getDispatchList();
+		for (String s: dsList.keySet()) {
+			for (Integer i: dsList.get(s)) {
+				Service serv = this.services.get(i);
+				serv.setAvailable();
+				serv.setPath(null);
+			}
+		}
+		this.reallocateServices(inc);
+		inc.offRunning();
+	}
 	
 	// dynamic reassignment of the services after the incident is resolved
 	// The idea here is to check the number of services required in the current node. 
 	// if the node has just enough services for it currently then no need to reassign.
 	// if the node has excess then make a table an use the dijkster's algorithm similar the one used to look for closest services.
-	
-	
 	//check if the current node has excess services
-	private void reallocateServices(Node node) {
+	private void reallocateServices(Incident inc) {
+		Node node = inc.getNode();
 		Map< String, Integer> required = new HashMap<>(node.getRequiredServices());
 		Map<String, Map<Integer, Service>> services = new HashMap<>(node.getServices());
 		for (String i: services.keySet()) {
@@ -252,17 +318,14 @@ public class Environment {
 			String s = iterService.next();
 			Map<Integer, Service> serv = services.get(s);
 			Iterator<Integer> iterServ = serv.keySet().iterator();
-			while (iterServ.hasNext()) {
+			Integer num = required.get(s);
+			if (num == null || num <= 0) continue;
+			while (iterServ.hasNext() && num > 0) {
 				Service service = serv.get(iterServ.next());
 				if (service.getAvailability()) {
-					int num = required.get(s);
-					if (num > 0) {
-						iterServ.remove();
-						required.put(s, num-1);
-					}
-					else {
-						break;
-					}
+					
+					iterServ.remove();
+					num--;
 				}
 			}
 			if (serv.isEmpty()) {
@@ -270,10 +333,13 @@ public class Environment {
 			}
 			
 		}
-		// initialises a starting node
-		NeighbourNode start = new NeighbourNode(node, 0);
+		
+
 		// A deep copy of the neighbouring node priority queue
-		PriorityQueue<NeighbourNode> queue = makeDuplicate(start, node.getNeighbour());
+		PriorityQueue<NeighbourNode> hp = new PriorityQueue<>(
+				(a, b) -> Float.compare(a.getDist(), b.getDist())
+				);
+		PriorityQueue<NeighbourNode> queue = makeDuplicate(new NeighbourNode(node, 0), hp);
 		// HashSet to keep track of the visited nodes
 		HashSet<Integer> visited = new HashSet<>();
 		// loop should end wither when all the services are allocated or when the queue is empty
@@ -282,6 +348,8 @@ public class Environment {
 			NeighbourNode node1 = queue.poll();
 			// check if its already in the visited list
 			if (visited.contains(node1.getNode().getID())) continue;
+			// add to visisted
+			visited.add(node1.getNode().getID());
 			// get the needed services 
 			Map<String, Integer> req = node1.getNode().getNeededServices();
 			// loop through the Map of list of services
@@ -289,15 +357,16 @@ public class Environment {
 					Map<Integer, Service> serv = services.get(s);
 					if (serv == null) continue;
 					Iterator<Integer> iter = serv.keySet().iterator();
-					while(iter.hasNext() && req.get(s) > 0) {
-						
+					int num = req.get(s);
+					while(iter.hasNext() && num > 0) {
 						// get an service from serv then removee it 
 						int key = iter.next();
 						Service curr = serv.get(key);
-						curr.setPath(flipPath(node1));
+						if (!curr.getAvailability()) continue;
+						curr.reallocate(flipPath(node1));
 						iter.remove();
 						// decrement the req.get(s) by 1
-						req.put(s, req.get(s)-1);
+						num --;
 					}
 					// checks if the services is empty after the iteration.
 					if (serv.isEmpty()) {
@@ -308,7 +377,7 @@ public class Environment {
 			this.makeDuplicate(node1, queue);
 			
 		}
-		
+
 	}
 	
 	
