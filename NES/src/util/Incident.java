0241858d393a51b241f5e40581164b402f757601
Yugen1121 <yugenlimbu777@gmail.com>
2025-12-19 13:38:09 +0000

    added documentation. final.

--- NES/src/util/Incident.java
+++ NES/src/util/Incident.java
@@ -2,24 +2,63 @@ package util;
 import java.util.Map;
 import util.nodes.Node;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Incident {
+	/** stores the incident id **/
 	private final int id;
-	private final int nodeId;
-	private final Node node;
-	private int incidentLevel;
-	private int incidentType;
-	private Map<String, ArrayList<Integer>> dispatchList;
-	private boolean incidentOn = true;
 	
+	/** stores the node id where the incident took place **/
+	private final int nodeId;
+	
+	/** stores the node where the incident took place **/
+	private final Node node;
+	
+	/** stores the incident level **/
+	private int incidentLevel;
+	
+	/** stores weather the incident is running or not **/
+	private Boolean running = true;
+	
+	/** stores the incident type **/
+	private int incidentType;
+	
+	/** a map of Service type pointing to a array of service id used too store the info of all the dispatched service**/
+	private Map<String, ArrayList<Integer>> dispatchList = new HashMap<>();
+	
+	/**
+	 * a constructor
+	 * @param id incident id
+	 * @param node node where the incident took place
+	 * @param incidentLevel incident level
+	 * @param incidentType type
+	 */
 	public Incident(int id, Node node, int incidentLevel, int incidentType) {
 		this.id = id;
 		this.nodeId = node.getID();
 		this.node = node;
 		this.incidentType = incidentType;
 		this.incidentLevel = incidentLevel;
+		dispatchList.put(Police.Type, new ArrayList<Integer>());
+		dispatchList.put(Ambulance.Type, new ArrayList<Integer>());
+		dispatchList.put(FireTruck.Type, new ArrayList<Integer>());
 	}
 	
+	/** returns weather the incident is ongoing **/
+	public Boolean getRunning() {
+		return running;
+	}
+	
+	/** sets teh incident of **/
+	public void offRunning() {
+		this.running = false;
+	}
+	
+	/**
+	 * adds a service to dispatch list
+	 * @param type of service
+	 * @param id id of the service
+	 */
 	public void addToDispatchList(String type, int id) {
 		ArrayList<Integer> list = this.dispatchList.get(type);
 		if(list != null) {	
@@ -34,48 +73,63 @@ public class Incident {
 	}
 	
 	
-	
+	/**
+	 * 
+	 * @return incident id
+	 */
 	public int getId() {
 		return this.id;
 	}
 	
+	/**
+	 * 
+	 * @return node id of the node where incident took place
+	 */
 	public int getNodeId() {
 		return this.nodeId;
 	}
 
+	/**
+	 * 
+	 * @return node where the incident took place
+	 */
 	public Node getNode() {
 		return this.node;
 	}
 
+	/**
+	 * 
+	 * @return incident level
+	 */
 	public int getIncidentLevel() {
 		return this.incidentLevel;
 	}
 
+	/**
+	 * 
+	 * @return incident type in string form
+	 */
 	public String getType() {
-		if (this.incidentType == 1) {
-			return "Polic";
+		if (this.incidentType == 3) {
+			return Police.Type;
 		}
 		else if(this.incidentType == 2) {
-			return "Ambulance";
+			return Ambulance.Type;
 		}
-		else if(this.incidentType == 3){
-			return "FireTruck";
+		else if(this.incidentType == 1){
+			return FireTruck.Type;
 		}
 		else {
-			return "Pollice";
+			return Police.Type;
 		}
 	}
 	
+	/**
+	 * 
+	 * @return the dispatch list
+	 */
 	public Map<String, ArrayList<Integer>> getDispatchList(){
 		return this.dispatchList;
-	}
-	
-	public boolean getIncidentOn() {
-		return this.incidentOn;
-	}
-	
-	public void setIncidentOff() {
-		this.incidentOn = false;
 	}
 
 }
